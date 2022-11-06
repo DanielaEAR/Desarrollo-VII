1c94211c60903288e4a36f561e5ac3bb986a99b5
Daniela A <daniela_3102@outlook.com>
2022-11-06 17:35:39 -0500

    Agregando Lab 20

---
+++ Lab20/Lab201.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <title>Laboratorio 20</title>
+</head>
+<body>
+    <form action = "Lab201.php" method ="Post">
+        Nombre: <input type = "text" name= "nombre" required><br>
+        Apellido: <input type = "text" name= "apellido" required><br>
+        Email: <input type = "email" name= "email"><br>
+        Edad: <input type = "number" name= "edad" min="0" max= "120"><br><br>
+        <input type = "submit" name = "guardar" value = "Guardar datos">
+    </form>
+<?php
+    include("UsuariosMDB.php");
+    $usrs = new UsuariosMDB();
+
+    if(array_key_exists('guardar', $_POST)){
+        $usrs ->insertarRegistro($_REQUEST['nombre'], $_REQUEST['apellido'], $_REQUEST['email'], $_REQUEST['edad']);
+        echo "Registro insertado exitosamente <br><br>";
+    }
+    echo "Registros en la colección usuarios: <br>";
+    $usrs->obtenerRegistros();
+?>
+</body>
+
